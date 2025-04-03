1b387e28420cbe0467ad8a3b62d3b7bac0144449
Dudous <sampaioeeduardo36@gmail.com>
2025-04-03 16:09:00 -0300

    login with jwt

---
+++ src/routes/auth.ts
@@ -0,0 +1,11 @@
+import express, { Router } from "express";
+import { authMiddleware } from "../middlewares/authMiddleware.ts";
+import AuthController from "../controllers/AuthController.ts";
+
+const router: Router = express.Router();
+
+router
+    .post('/register', AuthController.register)
+    .post('/login', AuthController.login)
+
+export default router
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,6 +3,7 @@ import express from 'express'
 import customer from './customer.ts'
 import product from './product.ts'
 import order from './order.ts'
+import auth from './auth.ts'
 
 export default function (app: Express) {
     app
@@ -10,4 +11,6 @@ export default function (app: Express) {
     .use('/customers', customer)
     .use('/products', product)
     .use('/orders', order)
+    .use('/auth', auth)
+
 }
